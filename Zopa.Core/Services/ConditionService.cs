d1c9fb6e537fd93857ed81e22e61db91514e3166
Bodrul <bodrul.ic@msn.com>
2019-08-10 09:56:50 +0100

    - Added logger to condition service. - Added condition service test.

--- Zopa.Core/Services/ConditionService.cs
+++ Zopa.Core/Services/ConditionService.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Zopa.Core.Contracts;
 
 namespace Zopa.Core.Services
 {
     public class ConditionService : IConditionService
     {
+        private readonly ILogger<ConditionService> _logger;
         private readonly int _divisibleValue;
         private readonly int _acceptanceRangeBottom;
         private readonly int _acceptanceRangeTop;
 
-        public ConditionService(IConfiguration configuration)
+        public ConditionService(ILogger<ConditionService> logger, IConfiguration configuration)
         {
+            _logger = logger;
             _divisibleValue = configuration.GetValue<int>("DivisibleValue");
             _acceptanceRangeBottom = configuration.GetValue<int>("AcceptanceRangeBottom");
             _acceptanceRangeTop = configuration.GetValue<int>("AcceptanceRangeTop");
@@ -19,16 +23,45 @@ namespace Zopa.Core.Services
         /// <inheritdoc />
         public bool CheckAmountRequestedValid(int amountRequested)
         {
-            bool isDivisible = IsDivisible(amountRequested);
-            bool isWithinRange = IsWithinRange(amountRequested);
-            return isDivisible && isWithinRange;
+            try
+            {
+                bool isDivisible = IsDivisible(amountRequested);
+                bool isWithinRange = IsWithinRange(amountRequested);
+                return isDivisible && isWithinRange;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Could not validate conditions for amount requested.", amountRequested, ex);
+                throw;
+            }
         }
 
         /// <inheritdoc />
-        public bool IsDivisible(int amountRequested) => amountRequested % _divisibleValue == 0;
+        public bool IsDivisible(int amountRequested)
+        {
+            try
+            {
+                return amountRequested % _divisibleValue == 0;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Could not verify divisible.", amountRequested, _divisibleValue, ex);
+                throw;
+            }
+        }
 
         /// <inheritdoc />
         public bool IsWithinRange(int amountRequested)
-            => amountRequested >= _acceptanceRangeBottom && amountRequested <= _acceptanceRangeTop;
+        {
+            try
+            {
+                return amountRequested >= _acceptanceRangeBottom && amountRequested <= _acceptanceRangeTop;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Could not verify range.", amountRequested, _acceptanceRangeBottom, _acceptanceRangeTop, ex);
+                throw;
+            }
+        }
     }
 }
---
+++ Zopa.UnitTests/Services/ConditionServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Zopa.Core.Contracts;
+using Zopa.Core.Services;
+
+namespace Zopa.UnitTests.Services
+{
+    [TestFixture]
+    public class ConditionServiceTests
+    {
+        private IConditionService _conditionService;
+        private Mock<ILogger<ConditionService>> _logger;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _logger = new Mock<ILogger<ConditionService>>();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown() => _conditionService = null;
+
+        [Test]
+        public void IsDivisible_UseDivisible_ReturnsTrue()
+        {
+            #region Arrange
+
+            var settings = new Dictionary<string, string> { { "DivisibleValue", "100" } };
+
+            IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection(settings).Build();
+            _conditionService = new ConditionService(_logger.Object, configuration);
+
+            #endregion
+
+            #region Act
+
+            var result = _conditionService.IsDivisible(200);
+
+            #endregion
+
+            #region Assert
+
+            Assert.That(result, Is.TypeOf<bool>());
+            Assert.That(result, Is.True);
+
+            #endregion
+        }
+    }
+}
