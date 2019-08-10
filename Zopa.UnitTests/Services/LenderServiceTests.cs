cca94edcb2e557fe2288be92a1923cd666b79016
Bodrul <bodrul.ic@msn.com>
2019-08-10 08:42:02 +0100

    Finished lender service tests.

--- Zopa.UnitTests/Services/LenderServiceTests.cs
+++ Zopa.UnitTests/Services/LenderServiceTests.cs
@@ -91,5 +91,73 @@ namespace Zopa.UnitTests.Services
 
             #endregion
         }
+
+        [Test]
+        public void GetListWithMinAmount_FiveAvailableLenders_ReturnsThreeValidLenders()
+        {
+            #region Arrange
+
+            var data = new List<Lender>
+            {
+                new Lender("Test1",1,100),
+                new Lender("Test2",2,200),
+                new Lender("Test3",3,1000),
+                new Lender("Test4",4,2500),
+                new Lender("Test5",5,10000)
+            };
+
+            _repository.Setup(x => x.GetAll()).Returns(data);
+            _lenderService = new LenderService(_logger.Object, _repository.Object);
+
+            #endregion
+
+            #region Act
+
+            var result = _lenderService.GetListWithMinAmount(1000);
+
+            #endregion
+
+            #region Assert
+
+            var list = result.ToList();
+            Assert.That(list, Is.TypeOf<List<Lender>>());
+            Assert.That(list.Count, Is.EqualTo(3));
+
+            #endregion
+        }
+
+        [Test]
+        public void GetListWithMinAmount_EmptyCollection_ThrowsException()
+        {
+            #region Arrange
+
+            _repository.Setup(x => x.GetAll()).Returns(new List<Lender>());
+            _lenderService = new LenderService(_logger.Object, _repository.Object);
+
+            #endregion
+
+            #region Act And Assert
+
+            Assert.That(() => _lenderService.GetListWithMinAmount(1000), Throws.Exception);
+
+            #endregion
+        }
+
+        [Test]
+        public void GetListWithMinAmount_NullResult_ThrowsException()
+        {
+            #region Arrange
+
+            _repository.Setup(x => x.GetAll()).Returns(() => null);
+            _lenderService = new LenderService(_logger.Object, _repository.Object);
+
+            #endregion
+
+            #region Act And Assert
+
+            Assert.That(() => _lenderService.GetListWithMinAmount(1000), Throws.Exception);
+
+            #endregion
+        }
     }
 }
