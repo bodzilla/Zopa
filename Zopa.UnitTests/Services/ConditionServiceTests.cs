5642c71d2639967d73ee040c053dbe5bcde93b54
Bodrul <bodrul.ic@msn.com>
2019-08-10 10:15:20 +0100

    - Added some condition service tests. - Code cleanup.

--- Zopa.UnitTests/Services/ConditionServiceTests.cs
+++ Zopa.UnitTests/Services/ConditionServiceTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -16,20 +15,61 @@ namespace Zopa.UnitTests.Services
         private Mock<ILogger<ConditionService>> _logger;
 
         [OneTimeSetUp]
-        public void Setup()
-        {
-            _logger = new Mock<ILogger<ConditionService>>();
-        }
+        public void Setup() => _logger = new Mock<ILogger<ConditionService>>();
 
         [OneTimeTearDown]
         public void TearDown() => _conditionService = null;
 
-        [Test]
-        public void IsDivisible_UseDivisible_ReturnsTrue()
+        [TestCase("100", 200, true)]
+        [TestCase("10", 20, true)]
+        [TestCase("1", 2, true)]
+        [TestCase("100", 201, false)]
+        [TestCase("10", 21, false)]
+        [TestCase("5", 1, false)]
+        public void IsDivisible_UseDivisible_ReturnsExpectedResult(string divisibleValue, int amountRequested, bool expectedResult)
         {
             #region Arrange
 
-            var settings = new Dictionary<string, string> { { "DivisibleValue", "100" } };
+            var settings = new Dictionary<string, string> { { "DivisibleValue", divisibleValue } };
+            IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection(settings).Build();
+            _conditionService = new ConditionService(_logger.Object, configuration);
+
+            #endregion
+
+            #region Act
+
+            var result = _conditionService.IsDivisible(amountRequested);
+
+            #endregion
+
+            #region Assert
+
+            Assert.That(result, Is.TypeOf<bool>());
+            Assert.That(result, Is.EqualTo(expectedResult));
+
+            #endregion
+        }
+
+        [TestCase("100", "500", 200, true)]
+        [TestCase("-500", "-100", -200, true)]
+        [TestCase("100", "500", 100, true)]
+        [TestCase("100", "500", 500, true)]
+        [TestCase("100", "500", 99, false)]
+        [TestCase("100", "500", 501, false)]
+        [TestCase("100", "500", -100, false)]
+        public void IsWithinRange_UseAcceptanceRanges_ReturnsExpectedResult(string acceptanceRangeBottom, string acceptanceRangeTop, int amountRequested, bool expectedResult)
+        {
+            #region Arrange
+
+            var settings = new Dictionary<string, string>
+            {
+                {
+                    "AcceptanceRangeBottom", acceptanceRangeBottom
+                },
+                {
+                    "AcceptanceRangeTop", acceptanceRangeTop
+                }
+            };
 
             IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection(settings).Build();
             _conditionService = new ConditionService(_logger.Object, configuration);
@@ -38,14 +78,14 @@ namespace Zopa.UnitTests.Services
 
             #region Act
 
-            var result = _conditionService.IsDivisible(200);
+            var result = _conditionService.IsWithinRange(amountRequested);
 
             #endregion
 
             #region Assert
 
             Assert.That(result, Is.TypeOf<bool>());
-            Assert.That(result, Is.True);
+            Assert.That(result, Is.EqualTo(expectedResult));
 
             #endregion
         }
